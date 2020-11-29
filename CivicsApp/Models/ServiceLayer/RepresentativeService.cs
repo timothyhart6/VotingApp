2ff279638a6bee72f6265bd29cf4b4dea84f9a6b
Timothy Hart <timothyhart6@gmail.com>
2020-11-28 23:11:08 -0500

    Fix URL issue. Need to move API Key from URL.

--- CivicsApp/Models/ServiceLayer/RepresentativeService.cs
+++ CivicsApp/Models/ServiceLayer/RepresentativeService.cs
@@ -64,21 +64,21 @@ namespace CivicsApp.Models
             
             var HouseMemberUrl = $"https://api.propublica.org/congress/v1/members/house/{state}/{district}/current.json";
             var SenateMembersUrl = $"https://api.propublica.org/congress/v1/members/senate/{state}/current.json";
-            //var GoogleRepresentativesUrl = $"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address={address}{zipcode}";
+            var GoogleRepresentativesUrl = $"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address={address}{zipcode}";
 
-            var GoogleRepresentativesUrl = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address=815 n high st";
+            //var GoogleRepresentativesUrl = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address=815 n high st";
 
-            HttpResponseMessage GoogleRepresentativesResults;
+            //HttpResponseMessage GoogleRepresentativesResults;
 
-            using (var request = new HttpRequestMessage(new HttpMethod("GET"), GoogleRepresentativesUrl))
-            {
-                GoogleRepresentativesResults = await httpClient.SendAsync(request);
-            }
+            //using (var request = new HttpRequestMessage(new HttpMethod("GET"), GoogleRepresentativesUrl))
+            //{
+            //    GoogleRepresentativesResults = await httpClient.SendAsync(request);
+            //}
 
 
             var HouseMemberResults = await httpClient.GetAsync(HouseMemberUrl);
             var SenateMembersResults = await httpClient.GetAsync(SenateMembersUrl);
-            //var GoogleRepresentativesResults = await httpClient.GetAsync(GoogleRepresentativesUrl);
+            var GoogleRepresentativesResults = await httpClient.GetAsync(GoogleRepresentativesUrl);
 
             var StringResultsForHouseMember = await HouseMemberResults.Content.ReadAsStringAsync();
             var StringResultsForSenateMembers = await SenateMembersResults.Content.ReadAsStringAsync();
