efd37c018ab2472dd089c1d26702e2812a3fc525
Timothy Hart <timothyhart6@gmail.com>
2020-12-14 16:08:57 -0500

    Trying to break out service call

--- CivicsApp/Models/ServiceLayer/RepresentativeService.cs
+++ CivicsApp/Models/ServiceLayer/RepresentativeService.cs
@@ -57,38 +57,50 @@ namespace CivicsApp.Models
             return ListOfRepresentatives;
         }
 
-        public async Task<CurrentDistrictRepresentatives> ListStateRepresentativesAsync(string address, string city, string state, string zipcode, string district)
+        public async Task<HouseMemberResult> FetchProPublicaHouseMember(HttpClient httpClient, string state, string district)
+        {
+            var HouseMemberUrl = $"https://api.propublica.org/congress/v1/members/house/{state}/{district}/current.json";
+            var HouseMemberResults = await httpClient.GetAsync(HouseMemberUrl);
+            var StringResultsForHouseMember = await HouseMemberResults.Content.ReadAsStringAsync();
+            var ProPublicaHouseMember = JsonConvert.DeserializeObject<PropublicaApiHouseMember>(StringResultsForHouseMember);
+
+            return ProPublicaHouseMember.Results[0];
+        }
+
+        public async Task<ProPublicaApiSenatorsForAState> FetchPropublicaSenators(HttpClient httpClient, string state)
+        {
+            var SenateMembersUrl = $"https://api.propublica.org/congress/v1/members/senate/{state}/current.json";
+            var SenateMembersResults = httpClient.GetAsync(SenateMembersUrl);
+            var StringResultsForSenateMembers = await SenateMembersResults.Content.ReadAsStringAsync();
+            var ProPublicaSenators = JsonConvert.DeserializeObject<ProPublicaApiSenatorsForAState>(StringResultsForSenateMembers);
+
+            return ProPublicaSenators;
+        }
+
+        public async Task<GoogleApiRepresentatives> FetchGoogleRepresentatives(HttpClient httpClient, string address, string zipCode)
+        {
+            var GoogleRepresentativesUrl = $"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address={address}{zipCode}";
+            var GoogleRepresentativesResults = httpClient.GetAsync(GoogleRepresentativesUrl);
+            var StringResultsForGoogleRepresentatives = GoogleRepresentativesResults.ToString();  //.Content.ReadAsStringAsync();
+            var GoogleRepresentatives = JsonConvert.DeserializeObject<GoogleApiRepresentatives>(StringResultsForGoogleRepresentatives);
+
+            return GoogleRepresentatives;
+        }
+
+        public async Task<CurrentDistrictRepresentatives> ListStateRepresentativesAsync(string address, string city, string state, string zipCode, string district)
         {
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("X-API-Key", "hxY9fxmPmO7Ev1UT6KUlbYaPVKM5v619B2DWRjIY");
-            
-            var HouseMemberUrl = $"https://api.propublica.org/congress/v1/members/house/{state}/{district}/current.json";
-            var SenateMembersUrl = $"https://api.propublica.org/congress/v1/members/senate/{state}/current.json";
-            var GoogleRepresentativesUrl = $"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCZom8UkHqmSzLcAWfnfL41vOfirikaS3w&address={address}{zipcode}";
 
-            //HttpResponseMessage GoogleRepresentativesResults;
+            var propublicaHouseMember = FetchProPublicaHouseMember(httpClient, state, district);
+            var propublicaSenators = FetchPropublicaSenators(httpClient, state);
+            var googleRepresentatives = FetchGoogleRepresentatives(httpClient, address, zipCode);
 
-            //using (var request = new HttpRequestMessage(new HttpMethod("GET"), GoogleRepresentativesUrl))
-            //{
-            //    GoogleRepresentativesResults = await httpClient.SendAsync(request);
-            //}
+            await Task.WhenAll(propublicaHouseMember, propublicaSenators, googleRepresentatives);
 
-
-            var HouseMemberResults = await httpClient.GetAsync(HouseMemberUrl);
-            var SenateMembersResults = await httpClient.GetAsync(SenateMembersUrl);
-            var GoogleRepresentativesResults = await httpClient.GetAsync(GoogleRepresentativesUrl);
-
-            var StringResultsForHouseMember = await HouseMemberResults.Content.ReadAsStringAsync();
-            var StringResultsForSenateMembers = await SenateMembersResults.Content.ReadAsStringAsync();
-            var StringResultsForGoogleRepresentatives = await GoogleRepresentativesResults.Content.ReadAsStringAsync();
-
-            var ProPublicaHouseMember = JsonConvert.DeserializeObject<PropublicaApiHouseMember>(StringResultsForHouseMember);
-            var ProPublicaSenators = JsonConvert.DeserializeObject<ProPublicaApiSenatorsForAState>(StringResultsForSenateMembers);
-            var GoogleRepresentatives = JsonConvert.DeserializeObject<GoogleApiRepresentatives>(StringResultsForGoogleRepresentatives); 
-
-            var Senator1 = SenatorAdapter.ConvertToSenatorObject(ProPublicaSenators.Results[0]);
-            var Senator2 = SenatorAdapter.ConvertToSenatorObject(ProPublicaSenators.Results[1]);
-            var HouseMember = HouseMemberAdapter.ConvertToHouseMemeberObject(ProPublicaHouseMember.Results[0], GoogleRepresentatives);
+            var Senator1 = SenatorAdapter.ConvertToSenatorObject(propublicaSenators.Results[0]);
+            var Senator2 = SenatorAdapter.ConvertToSenatorObject(propublicaSenators.Results[1]);
+            var HouseMember = HouseMemberAdapter.ConvertToHouseMemeberObject(proPublicaHouseMember, GoogleRepresentatives);
 
             var DistrictReps = new CurrentDistrictRepresentatives(Senator1, Senator2, HouseMember);
 
